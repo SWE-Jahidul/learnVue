27f0735737a77aed397bd00cabdc465b11bdc4a3
SWE_jahidul <programmerjahid162@gmail.com>
2019-01-29 17:18:57 +0600

    keybord handerler and 2 way data binding

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 new Vue({
     el: "#veu-app",
     data: {
-        age: 22,
-        x:0,
-        y:0
+        name :"",
+        age:""
     },
     methods: {
 
-        Add: function (inc) {
-              this.age +=inc;
-        },
-
-        substract:function(dec){
-            this.age -=dec;
-        },
-
-        updatexy:function(event)
+        LogName:function()
         {
-        this.x =event.offsetX;
-        this.y =event.offsetY;            
+            this.name;
+            
         },
-        click:function()
+        LogAge:function()
         {
-            alert("you click me ")
+         this.age;            
         }
     }
 
