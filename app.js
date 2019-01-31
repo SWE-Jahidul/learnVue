7f9e5bac234635ac0d297ad2ac904b57de02f64a
SWE_jahidul <programmerjahid162@gmail.com>
2019-01-31 18:02:56 +0600

    componetn works

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
+Vue.component('greeting', {
+    template: '<p> hey there , i am a {{name}}. <button v-on:click="changeName">change name </button> </p>',
+    data: function () {
+
+        return {
+            name: "jimu"
+        }
+    },
+
+    methods:
+    {
+        changeName:function() {
+            this.name = "mario"
+
+        }
+    }
+
+
+
+})
+
+
+
+
 var one = new Vue({
     el: "#veu-app-one",
     data: {
-        title: 'vue App one '
+
     },
-    methods: {
 
-        
-
-    }
-    ,
-    computed: {
-        greet:function() {
-            return "hellow from app one ";
-        }
-    }
 });
 
 
 var two = new Vue({
     el: "#veu-app-two",
     data: {
-        title: 'vue App Two '
 
     },
-    methods: {
-
-        changetitle:function(){
-            one.title = 'title change '
-        }
-    }
-    ,
-    computed: {
-
-        greet:function() {
-            return "hellow from app two ";
-        }
 
 
-    }
 });
-
-
-two.title = "change from outside"
